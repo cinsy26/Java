3d4cb592e22a4bb80944f764ccf5530f71f57a51
cinsy26 <143693285+cinsy26@users.noreply.github.com>
2024-09-11 10:26:49 +0900

    [Bronze IV] Title: Identifying tea, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/11549. Identifying tea/Identifying tea.java
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -1,18 +1,27 @@
+
 import java.util.Scanner;
 
-public class Main{
-	public static void main(String[] args){
+public class Main {
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int sum = 0;
-		
-		for(int i = 0; i < 5; i++){
-			int A = sc.nextInt();
-			if(N==A){
-				sum++;
+		int T = sc.nextInt(); // 차 종류
+		int[] arr = new int[5];
+
+		//참가자 정답 입력
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		// 정답자 카운트해주기
+		int cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == T) {
+				cnt++;
 			}
 		}
-		System.out.println(sum);
+		System.out.println(cnt);
+		sc.close();
 	}
-};
+
+}
