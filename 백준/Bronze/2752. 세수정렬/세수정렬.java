0865865dad58ffc8321db9900cfd50479238a5d9
cinsy26 <143693285+cinsy26@users.noreply.github.com>
2024-09-13 13:52:19 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 16364 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main{
-	public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	
-	int A = sc.nextInt();
-	int B = sc.nextInt();
-	int C = sc.nextInt();
-	
-	int[] list = new int [] {A, B, C};
-	
-	Arrays.sort(list);
-	
-  System.out.println(list[0] + " " + list[1] + " " + list[2]);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = br.readLine().split(" ");
+		
+		int A = Integer.parseInt(input[0]);
+		int B = Integer.parseInt(input[1]);
+		int C = Integer.parseInt(input[2]);
+		
+		int[] list = new int[] {A, B, C};
+		
+		Arrays.sort(list);
+		
+		System.out.println(list[0] + " " + list[1] + " " + list[2]);
+		}
 	}
-}
