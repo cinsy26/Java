0c938518d50e015a2fa448e62b54f0e24a986ac5
cinsy26 <143693285+cinsy26@users.noreply.github.com>
2024-09-19 12:50:50 +0900

    [Bronze IV] Title: Hello Judge, Time: 180 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i < N; i++){
+			int A = i + 1;
+			System.out.println("Hello World, Judge " + A + "!");
+		}
+	}
+}
