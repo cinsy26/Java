a2b104d4c18ea933ba066706a32f2bb80af882e8
cinsy26 <143693285+cinsy26@users.noreply.github.com>
2025-08-27 14:49:25 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 120 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,17 +1,24 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[26];
-        String s = br.readLine();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            arr[c-97]++;
+        String[] S = br.readLine().split("");
+        
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"};
+        int [] answer = new int[26];
+        
+        for(int i = 0; i<26; i++){
+            for(int j = 0; j<S.length; j++){
+                if(S[j].equals(alphabet[i])){
+                    answer[i] = answer[i] +1;
+                }
+            }
         }
-        for(int i = 0; i < 26; i++){
-		        System.out.print(arr[i] + " ");
-		     }
-    }
+        
+        for(int l = 0; l<26; l++){
+            System.out.print(answer[l]+" ");
+        }
+    } 
 }
